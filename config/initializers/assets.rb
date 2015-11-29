6d9c5294bd1a73884c33c50ac63cbeee9a092a81
shulihallak <shuli@invisiblenetworks.co>
2015-11-29 13:34:45 -0500

    removed unused assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( search.js app.css app.js jquery.ui.widget.js jquery.iframe-transport.js jquery.fileupload.js jquery.cloudinary.js vegas.js vegas.css)
+Rails.application.config.assets.precompile += %w( search.js app.css app.js)
